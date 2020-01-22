0bc47d49eef4be7194f1750f5cad3c9c83d00c20
Mathew Gitata <mathewgitata@gmail.com>
2020-01-22 15:26:52 +0300

    Set up the action bar

--- app/src/main/java/ke/co/gitata/ibake/HomePageActivity.java
+++ app/src/main/java/ke/co/gitata/ibake/HomePageActivity.java
@@ -1,8 +1,11 @@
 package ke.co.gitata.ibake;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
